feeddcacb4b227e7dee7edb249577354a603b648
Nadia Dębiec <nadi337@gmail.com>
2018-12-05 14:26:05 +0000

    Added UserStock association and views: listing and deleting

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 root 'welcome#index'  
 get 'my_portfolio', to: 'users#my_portfolio'
 get 'search_stocks', to: 'stocks#search'
+resources :user_stocks, only: [:create, :destroy]
+#post 'user_stocks', to: 'user_stocks#create'
+#delete 'user_stocks/:id', to: 'user_stocks#destroy'
 
 end
